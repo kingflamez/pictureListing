7ec8c855972db4f527b9c718a669bcff209f026b
Flamez - Oluwole Adebiyi <flamekeed@gmail.com>
2019-11-30 07:21:03 +0100

    Initial Commit

---
+++ project1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  project1
+//
+//  Created by Oluwole Adebiyi on 30/11/2019.
+//  Copyright © 2019 Oluwole Adebiyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
